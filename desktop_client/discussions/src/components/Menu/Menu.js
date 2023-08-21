4329c25f450f77c63b4aab8d20e4d19a2ca9beaa
MyriamKiriakos342 <myriamkiriakos342@gmail.com>
2023-08-21 09:31:42 -0400

    MK added the Menu component

---
+++ desktop_client/discussions/src/components/Menu/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { MenuWrapper } from './Menu.styled';
+
+const Menu = () => (
+ <MenuWrapper>
+    Menu Component
+ </MenuWrapper>
+);
+
+Menu.propTypes = {};
+
+Menu.defaultProps = {};
+
+export default Menu;
