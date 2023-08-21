d84b04a1467fe803d4b8a81f4aa1f35ef377bd18
MyriamKiriakos342 <myriamkiriakos342@gmail.com>
2023-08-21 09:34:19 -0400

    MK added the search bar component

---
+++ desktop_client/discussions/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SearchBarWrapper } from './SearchBar.styled';
+
+const SearchBar = () => (
+ <SearchBarWrapper>
+    SearchBar Component
+ </SearchBarWrapper>
+);
+
+SearchBar.propTypes = {};
+
+SearchBar.defaultProps = {};
+
+export default SearchBar;
